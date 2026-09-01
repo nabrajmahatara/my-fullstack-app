c61c6d0d929ae688c85b00c77aeb6db4c84d591b
nabrajmahatara <nabrajmahatara0@gmail.com>
2026-09-01 17:30:26 +0545

    feat: implement user schema with roles and priorities constants

--- koms-backend/src/modules/user/user.model.js
+++ koms-backend/src/modules/user/user.model.js
@@ -1,6 +1,7 @@
 import { Timestamp } from "mongodb";
 import bcrypt from "bcryptjs";
 import mongoose, { Schema } from "mongoose";
+import { ROLES,ROLE_VALUES } from "../../constants/roles.js";
 
 const {Schema}= mongoose;
 
@@ -22,34 +23,37 @@ const userSchema = new mongoose.Schema ({
         lowercase:true
     },
     password:{
-        trype:String,
+        type:String,
         required:[true, "password is required"],
-        unique:true,
         trim:true,
         minlength:[8,"atleast 8 unique characters required"]
     },
-    role:{
-        type:String,
-        enum:["user", "Admin"],
-        default:"user"
-    },
+    role: { 
+      type: String, 
+      enum: ROLE_VALUES,       
+      default: ROLES.MEMBER,   
+      required: true 
+    
+  },
+   
     isActive:{
         type:Boolean,
         default:true
     },
     
-    
-
-
 },{timestamps:true})
-Userschema.pre("save",async function () {
-    if (!this.isModified("password")) return;
+userSchema.pre("save",async function () {
+    if (!this.isModified("password")) return next();
+  try {
     const saltRound = 10;
-    const hashedPassword = await bcrypt.hash(this.password,saltRound);
-    this.password = hashedPassword
-})
-Userschema.methods.comparePassword = async function(candidatePassword){
+    this.password = await bcrypt.hash(this.password, saltRound);
+    next();
+  } catch (error) {
+    next(error); 
+  }
+});
+userSchema.methods.comparePassword = async function(candidatePassword){
     const isMatch = await bcrypt.compare(candidatePassword, this.password)
     return isMatch
 }
-export default mongoose.model("user", userSchema)
+export default mongoose.model("User", userSchema)
